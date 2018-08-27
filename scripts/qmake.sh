ff509871984772a571b8d46aaff75865c0e0feec
Michael Dockter <michael@dockter.com>
2018-08-27 18:25:33 -0400

    Savepoint

--- scripts/qmake.sh
+++ scripts/qmake.sh
@@ -5,5 +5,5 @@ echo "==========================================================================
 yum -y install \
    qt5-qtbase-devel
 
-mv /tmp/files/etc/profile.d/qmake.sh /etc/profile.d/qmake.sh
+# mv /tmp/files/etc/profile.d/qmake.sh /etc/profile.d/qmake.sh
 
